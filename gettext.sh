e622ef6478a2b633fe9fbf98d28e2fdb0d8fe477
Cimbali <me@cimba.li>
2016-10-24 14:55:09 +0200

    Script to update translatable strings

---
+++ gettext.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+p=`realpath $0`
+cd `dirname $p`
+pot=pympress/share/locale/pympress.pot
+translated=`ls pympress/share/locale/*/*.po`
+opts="--no-location --no-wrap --sort-output"
+pkg="--package-name=Pympress --package-version=`git describe --abbrev=0` --msgid-bugs-address=me@cimba.li"
+
+xgettext -d python $opts $pkg -L Python --from-code utf-8 --keyword=_ pympress/*.py
+xgettext -d glade  $opts $pkg -L glade --keyword=translatable pympress/share/*.glade
+
+sed -i 's/CHARSET/UTF-8/g' python.po glade.po
+
+msgcat $opts -t utf-8 -o temp.po $pot glade.po python.po && rm glade.po python.po
+sed '/^"#-#-#-#-#  pympress\.pot (Pympress v\S\+)  #-#-#-#-#\\n"$/d;/^"#-#-#-#-# .* #-#-#-#-#\\n"$/,/^$/d' temp.po > $pot && rm temp.po
+
+# gettext doesn't like .pot, only .po
+for t in $translated; do
+	msgmerge -U $opts $t $pot
+done
