33d6729184fcb0f16b4651e281cc928f45dd2b7a
Cimbali <me@cimba.li>
2017-05-17 17:50:39 +0200

    Update version number, docs

---
+++ docs/_sources/index.rst.txt
@@ -0,0 +1,17 @@
+Welcome to pympress's documentation!
+====================================
+
+Contents
+--------
+.. toctree::
+   :maxdepth: 2
+
+   README.md
+   pympress.md
+
+Indices and tables
+------------------
+
+* :ref:`genindex`
+* :ref:`modindex`
+
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -151,7 +151,6 @@ import sphinx_rtd_theme
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_theme_options = {
-    'collapse_navigation': True,
 }
 
 # Theme options are theme-specific and customize the look and feel of a theme
