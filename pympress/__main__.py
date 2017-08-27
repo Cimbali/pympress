58b9fb2cb63a1b05bbb32f625cdc845c908315de
Cimbali <me@cimba.li>
2017-08-27 21:47:56 +0200

    Import __future__.print_function also in __main__

--- pympress/__main__.py
+++ pympress/__main__.py
@@ -20,6 +20,8 @@
 #       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #       MA 02110-1301, USA.
 
+from __future__ import print_function
+
 import logging
 logger = logging.getLogger(__name__)
 
