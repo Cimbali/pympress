464c4d9df55196e27fc9881955dd77848e11fe58
Cimbali <me@cimba.li>
2018-02-09 15:15:32 +0100

    Update translation script and po/pot files
    Missing translations in German:
    	msgid "Invalid log level \"{}\", try one of {}"
    	msgid "Set level of verbosity in log file:"
    
    Fuzzy translations in German:
    	msgid "Not starting content or presenter window full screen because there is only one monitor"
    	msgstr "Inhalts- und Präsentator-Fenster nicht im Vollbild-Modus gestarted weil es nur ein Monitor gibt!"
    
    	msgid "Pointer"
    	msgstr "Zeiger"

--- gettext.sh
+++ gettext.sh
@@ -4,22 +4,32 @@ p=`realpath $0`
 cd `dirname $p`
 pot=pympress/share/locale/pympress.pot
 translated=`ls pympress/share/locale/*/*.po`
+version=`git describe --tags --abbrev=0`
 opts="--no-location --no-wrap --sort-output"
-pkg="--package-name=Pympress --package-version=`git describe --tags --abbrev=0` --msgid-bugs-address=me@cimba.li"
+pkg="--package-name=Pympress --package-version=${version} --msgid-bugs-address=me@cimba.li"
+pkg="--omit-header" # Don't re-output headers as we update files
 
 xgettext -d python $opts $pkg -L Python --from-code utf-8 --keyword=_ pympress/*.py
-xgettext -d glade  $opts $pkg -L glade --keyword=translatable pympress/share/xml/*.glade
+xgettext -d glade  $opts $pkg -L Glade --keyword=translatable pympress/share/xml/*.glade
 
 sed -i 's/CHARSET/UTF-8/g' python.po glade.po
 
-msgcat $opts -t utf-8 -o temp.po $pot glade.po python.po && rm glade.po python.po
-sed '/^"#-#-#-#-#  pympress\.pot (Pympress v\S\+)  #-#-#-#-#\\n"$/d;/^"#-#-#-#-# .* #-#-#-#-#\\n"$/,/^$/d' temp.po > $pot && rm temp.po
+msgcat $opts -t utf-8 -o temp.po $pot glade.po python.po && rm glade.po python.po && mv temp.po $pot
+
+# Do some updates and remove comments
+sed -i '/^msgid ""$/,$!d;/^"#-#-#-#-#  \(glade\|pympress\)\.pot (Pympress v\S\+)  #-#-#-#-#\\n"$/d' $pot
+sed -i "/Project-Id-Version: Pympress v/s/v[.0-9a-z]\\+/${version}/" $pot $translated
 
 # gettext doesn't like .pot, only .po
 for t in $translated; do
+	printf "Updating %s: " "$t"
 	msgmerge -U $opts $t $pot
-done
 
-# then after updating translations:
-# (cd `dirname $t` && msgfmt pympress.po -fo LC_MESSAGES/pympress.mo )
+	echo "Missing and fuzzy translations:"
+	sed -rn '1N;N;/^#, fuzzy/p;/\nmsgstr ""\n$/P;$s/\n(msgid ".*")\nmsgstr ""$/\1/p;D' $t
+	echo
+	echo after updating translations:
+	echo "msgfmt $t -fo `dirname $t`/LC_MESSAGES/pympress.mo"
+	echo
+done
 
