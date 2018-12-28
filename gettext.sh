29912ad7e50d2aa39dd153d8becaa343ef510f58
Cimbali <me@cimba.li>
2018-12-28 13:13:04 +0100

    Update translations strings and script, translate French
    Have the script remove outdated translation strings, find deeper python files.
    Remove outdated strings, add new ones, update French translation.

--- gettext.sh
+++ gettext.sh
@@ -9,12 +9,13 @@ opts="--no-location --no-wrap --sort-output"
 pkg="--package-name=Pympress --package-version=${version} --msgid-bugs-address=me@cimba.li"
 pkg="--omit-header" # Don't re-output headers as we update files
 
-xgettext -d python $opts $pkg -L Python --from-code utf-8 --keyword=_ pympress/*.py
+xgettext -d python $opts $pkg -L Python --from-code utf-8 --keyword=_ `find pympress/ -name "*.py"`
 xgettext -d glade  $opts $pkg -L Glade --keyword=translatable pympress/share/xml/*.glade
 
 sed -i 's/CHARSET/UTF-8/g' python.po glade.po
 
-msgcat $opts -t utf-8 -o temp.po $pot glade.po python.po && rm glade.po python.po && mv temp.po $pot
+sed '/^$/q' $pot > header.po
+msgcat $opts -t utf-8 -o $pot header.po glade.po python.po && rm glade.po python.po header.po
 
 # Do some updates and remove comments
 sed -i '/^msgid ""$/,$!d;/^"#-#-#-#-#  \(glade\|pympress\)\.pot (Pympress v\S\+)  #-#-#-#-#\\n"$/d' $pot
@@ -24,6 +25,7 @@ sed -i "/Project-Id-Version: Pympress v/s/v[.0-9a-z]\\+/${version}/" $pot $trans
 for t in $translated; do
 	printf "Updating %s: " "$t"
 	msgmerge -U $opts $t $pot
+	rm -f ${t}~
 
 	echo "Missing and fuzzy translations:"
 	sed -rn '1N;N;/^#, fuzzy/p;/\nmsgstr ""\n$/P;$s/\n(msgid ".*")\nmsgstr ""$/\1/p;D' $t
