ebda8ea1457fe513258ad37f84309c17d191b182
Cimbali <me@cimba.li>
2019-10-21 20:20:11 +0200

    Update first and relaunch the msys2 shell
    In case some msys2 dependencies are to be updated (eg. mintty), which
    then causes further upgrades to fails.
    
    Also add verbosity and exit-on-fail options to build script to help read
    the appveyor output.

--- scripts/build_msi_mingw.sh
+++ scripts/build_msi_mingw.sh
@@ -1,5 +1,6 @@
 # this script is meant to be run by appveyor on msys2 with mingw-w64
 # arch=x86_64 or i686, py=python2 or python3, vlc=with-vlc or without-vlc
+set -ve
 pacman -Syu --noprogressbar --noconfirm
 pacman -S --noprogressbar --noconfirm --needed base-devel mingw-w64-$arch-{gtk3,cairo,poppler,$py,gcc,vlc}
 pacman -S --noprogressbar --noconfirm --needed mingw-w64-$arch-$py-{pip,gobject,cairo,appdirs,packaging,cx_Freeze}
