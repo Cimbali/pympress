f2e60d82d99c9952444b8d20a11904abb66a8c5b
Cimbali <me@cimba.li>
2020-01-23 17:46:13 +0100

    Use appveyor image where updating pacman does not crash the whole build

--- scripts/build_msi_mingw.sh
+++ scripts/build_msi_mingw.sh
@@ -2,8 +2,8 @@
 # arch=x86_64 or i686, py=python2 or python3, vlc=with-vlc or without-vlc
 set -ve
 pacman -Syu --noprogressbar --noconfirm
-pacman -S --noprogressbar --noconfirm --needed base-devel mingw-w64-$arch-{gtk3,cairo,poppler,$py,gcc,vlc}
-pacman -S --noprogressbar --noconfirm --needed mingw-w64-$arch-$py-{pip,gobject,cairo,appdirs,packaging,cx_Freeze}
+pacman -S --noprogressbar --noconfirm --needed base-devel mingw-w64-$arch-gtk3 mingw-w64-$arch-cairo mingw-w64-$arch-poppler mingw-w64-$arch-$py mingw-w64-$arch-gcc mingw-w64-$arch-vlc
+pacman -S --noprogressbar --noconfirm --needed mingw-w64-$arch-$py-pip mingw-w64-$arch-$py-gobject mingw-w64-$arch-$py-cairo mingw-w64-$arch-$py-appdirs mingw-w64-$arch-$py-packaging mingw-w64-$arch-$py-cx_Freeze
 $py -m pip install --disable-pip-version-check --upgrade pip
 $py -m pip install watchdog python-vlc babel babelgladeextractor
 $py setup.py compile_catalog
