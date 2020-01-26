bee1009efb8da46feb850d9efd6affb81ff8b83e
Cimbali <me@cimba.li>
2020-01-26 14:00:03 +0100

    Use MSYS’s zip rather than 7zip

--- scripts/build_msi_mingw.sh
+++ scripts/build_msi_mingw.sh
@@ -2,7 +2,7 @@
 # arch=x86_64 or i686, py=python2 or python3, vlc=with-vlc or without-vlc
 set -ve
 pacman -Syu --noprogressbar --noconfirm
-pacman -S --noprogressbar --noconfirm --needed base-devel mingw-w64-$arch-gtk3 mingw-w64-$arch-cairo mingw-w64-$arch-poppler mingw-w64-$arch-$py mingw-w64-$arch-gcc mingw-w64-$arch-vlc
+pacman -S --noprogressbar --noconfirm --needed base-devel mingw-w64-$arch-gtk3 mingw-w64-$arch-cairo mingw-w64-$arch-poppler mingw-w64-$arch-$py mingw-w64-$arch-gcc mingw-w64-$arch-vlc zip
 pacman -S --noprogressbar --noconfirm --needed mingw-w64-$arch-$py-pip mingw-w64-$arch-$py-gobject mingw-w64-$arch-$py-cairo mingw-w64-$arch-$py-appdirs mingw-w64-$arch-$py-packaging mingw-w64-$arch-$py-cx_Freeze
 $py -m pip install --disable-pip-version-check --upgrade pip
 $py -m pip install watchdog python-vlc babel babelgladeextractor
@@ -14,4 +14,4 @@ $py setup.py --freeze --$vlc bdist_msi --add-to-path True --target-name pympress
 cd build
 mv exe.mingw-* pympress
 cp ../pympress/share/defaults.conf pympress/pympress.conf
-7z a -tzip -r -y ../dist/pympress-`git describe --tags --always`-$arch.zip pympress
+zip -r ../dist/pympress-`git describe --tags --always`-$arch.zip pympress
