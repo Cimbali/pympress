5a4d3173dc9540e0bb3bd641f3f46ffa94ca94cf
Cimbali <me@cimba.li>
2020-02-06 18:20:28 +0100

    Install a pympress icon and desktop file with pip
    As part of improving installs, #132. For this, also rename the 128px
    icon to simply pympress.png

--- docs/conf.py
+++ docs/conf.py
@@ -270,7 +270,7 @@ html_short_title = "Pympress v{}".format(version)
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 #
-html_logo = '../pympress/share/pixmaps/pympress-128.png'
+html_logo = '../pympress/share/pixmaps/pympress.png'
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
