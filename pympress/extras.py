Cimbali/pympress

commit 0c2dfbe0f8f2119fe955a44d3273d459a1716afc

Transform os.path to pathlib

pympress/extras.py
@@ -28,7 +28,7 @@ from __future__ import print_function, unicode_literals
 import logging
 logger = logging.getLogger(__name__)
 
-import os.path
+import pathlib
 import gi
 import cairo
 gi.require_version('Gtk', '3.0')
@@ -747,17 +747,16 @@ class FileWatcher(object):
         self.stop_watching()
 
         scheme, path = uri.split('://', 1)
-        path = url2pathname(path)
-        directory = os.path.dirname(path)
+        path = pathlib.Path(url2pathname(path))
         if scheme != 'file':
             logger.error('Impossible to watch files with {} schemes'.format(scheme), exc_info = True)
             return
 
         self.monitor.on_modified = lambda e: self._enqueue(callback, *args, **kwargs) if e.src_path == path else None
         try:
-            self.observer.schedule(self.monitor, directory)
+            self.observer.schedule(self.monitor, str(path.parent))
         except OSError:
-            logger.error('Impossible to open dir at {}'.format(directory), exc_info = True)
+            logger.error('Impossible to open dir at {}'.format(str(path.parent)), exc_info = True)
 
 
     def stop_watching(self):

pympress/config.py
@@ -26,8 +26,7 @@
 import logging
 logger = logging.getLogger(__name__)
 
-import os
-import shutil
+import pathlib
 import json
 from collections import deque
 
@@ -66,23 +65,27 @@ class Config(configparser.ConfigParser, object):  # python 2 fix
     shortcuts = {}
 
     @staticmethod
-    def path_to_config(search_legacy_locations = False):
+    def path_to_config(search_legacy_locations=False):
         """ Return the path to the currently used configuration file.
 
         Args:
             search_legacy_locations (`bool`): whether to look in previously used locations
+
+        Returns:
+            :class:`~pathlib.Path`: The path to the config file to use
         """
         portable_config = util.get_portable_config()
-        if os.path.exists(portable_config):
+        if portable_config.exists():
             return portable_config
 
         user_config = util.get_user_config()
 
         # migrate old configuration files from previously-used erroneous locations
-        if search_legacy_locations and (util.IS_POSIX or util.IS_MAC_OS) and not os.path.exists(user_config):
-            for legacy_location in [os.path.expanduser('~/.pympress'), os.path.expanduser('~/.config/pympress')]:
-                if os.path.exists(legacy_location):
-                    shutil.move(legacy_location, user_config)
+        if search_legacy_locations and (util.IS_POSIX or util.IS_MAC_OS) and not user_config.exists():
+            for legacy_location in ['~/.pympress', '~/.config/pympress']:
+                legacy_location = pathlib.Path(legacy_location).expanduser()
+                if legacy_location.exists():
+                    legacy_location.rename(user_config)
 
         return user_config
 
@@ -96,10 +99,11 @@ class Config(configparser.ConfigParser, object):  # python 2 fix
 
         No need to populate the new config file, this will be done on pympress exit.
         """
+        portable_config = util.get_portable_config()
         if Config.using_portable_config():
-            os.remove(util.get_portable_config())
+            portable_config.unlink()
         else:
-            with open(util.get_portable_config(), 'w'):
+            with open(portable_config, 'w'):
                 pass
 
 
@@ -110,7 +114,7 @@ class Config(configparser.ConfigParser, object):  # python 2 fix
         Returns:
             `bool`: `True` iff we are using the portable (i.e. in install dir) location
         """
-        return util.get_portable_config() == Config.path_to_config()
+        return util.get_portable_config().exists()
 
 
     def __init__(config):

pympress/document.py
@@ -38,7 +38,6 @@ from __future__ import print_function, unicode_literals
 import logging
 logger = logging.getLogger(__name__)
 
-import os
 import math
 import enum
 import pathlib
@@ -331,12 +330,12 @@ class Page(object):
                     continue
             elif annot_type == Poppler.AnnotType.FILE_ATTACHMENT:
                 attachment = annotation.annot.get_attachment()
-                prefix, ext = os.path.splitext(attachment.name)
-                with tempfile.NamedTemporaryFile('wb', suffix=ext, prefix=prefix, delete=False) as f:
+                filename = pathlib.Path(attachment.name)
+                with tempfile.NamedTemporaryFile('wb', suffix=filename.suffix, prefix=filename.stem, delete=False) as f:
                     # now the file name is shotgunned
-                    filename = f.name
+                    filename = pathlib.Path(f.name)
                     self.parent.remove_on_exit(filename)
-                if not attachment.save(filename):
+                if not attachment.save(str(filename)):
                     logger.error(_("Pympress can not extract attached file"))
                     continue
                 action = Link.build_closure(fileopen, filename)
@@ -474,9 +473,9 @@ class Page(object):
                 ext = get_extension(media.get_mime_type())
                 with tempfile.NamedTemporaryFile('wb', suffix=ext, prefix='pdf_embed_', delete=False) as f:
                     # now the file name is shotgunned
-                    filename = f.name
+                    filename = pathlib.Path(f.name)
                     self.parent.remove_on_exit(filename)
-                if not media.save(filename):
+                if not media.save(str(filename)):
                     logger.error(_("Pympress can not extract embedded media"))
                     return None
             else:
@@ -704,7 +703,7 @@ class Document(object):
     #: navigation in the document faster by avoiding calls to Poppler when loading
     #: a page that has already been loaded.
     pages_cache = {}
-    #: Files that are temporary and need to be removed
+    #: `set` of :class:`~pathlib.Path` representing the temporary files which need to be removed
     temp_files = set()
     #: History of pages we have visited
     history = []
@@ -1126,18 +1125,17 @@ class Document(object):
         """ Returns full path, extrapolated from a path relative to this document or to the current directory.
 
         Args:
-            filename (`str`):  Name of the file or relative path to it
+            filename (:class:`~pathlib.Path`):  Name of the file or relative path to it
 
         Returns:
-            `str`: the full path to the file or None if it doesn't exist
+            :class:`~pathlib.Path`: the full path to the file or None if it doesn't exist
         """
-        filepath = None
-        if os.path.isabs(filename):
-            return os.path.normpath(filename) if os.path.exists(filename) else None
+        if filename.is_absolute():
+            return filename.resolve() if filename.exists() else None
 
-        for d in [os.path.dirname(url2pathname(self.uri.split('://', 1)[1])), os.getcwd()]:
-            filepath = os.path.normpath(os.path.join(d, filename))
-            if os.path.exists(filepath):
+        for dirname in [pathlib.Path(url2pathname(urlsplit(self.uri).path)).parent, pathlib.Path.cwd()]:
+            filepath = dirname.joinpath(filename)
+            if filepath.exists():
                 return filepath
 
 
@@ -1145,7 +1143,7 @@ class Document(object):
         """ Remember a temporary file to delete later.
 
         Args:
-            filename (`str`): The path to the file to delete
+            filename (:class:`~pathlib.Path`): The path to the file to delete
         """
         self.temp_files.add(filename)
 
@@ -1154,7 +1152,8 @@ class Document(object):
         """ Removes all files that were extracted from the pdf into the filesystem.
         """
         for f in self.temp_files:
-            os.remove(f)
+            if f.exists():
+                f.unlink()
         self.temp_files.clear()
 
 

pympress/scribble.py
@@ -176,7 +176,7 @@ class Scribbler(builder.Builder):
         masks = [cairo.ImageSurface.create_from_png(util.get_icon_path('marker_fill_{}.png'.format(n))) for n in ms]
 
         self.marker_surfaces = list(zip(icons, masks))
-        self.eraser_surface = cairo.ImageSurface.create_from_png(util.get_icon_path('eraser.png'))
+        self.eraser_surface = cairo.ImageSurface.create_from_png(str(util.get_icon_path('eraser.png')))
 
         # Load color and active pen preferences. Pen 0 is the eraser.
         self.color_width = [(Gdk.RGBA(0, 0, 0, 0), 150)] + list(zip(

pympress/ui.py
@@ -36,10 +36,12 @@ from __future__ import print_function, unicode_literals
 import logging
 logger = logging.getLogger(__name__)
 
-import os.path
+import pathlib
 import math
 import sys
 import gc
+from urllib.request import url2pathname, pathname2url
+from urllib.parse import urlsplit, urlunsplit
 
 import gi
 import cairo
@@ -635,9 +637,9 @@ class UI(builder.Builder):
         about.set_copyright(_('Contributors:') + '\n' + pympress['contributors'])
         about.set_comments(_('pympress is a little PDF reader written in Python ' +
                              'using Poppler for PDF rendering and GTK for the GUI.\n') +
-                           _('Some preferences are saved in ') + self.config.path_to_config() + '\n' +
-                           _('Resources are loaded from ') + os.path.dirname(util.get_locale_dir()) + '\n' +
-                           _('The log is written to ') + util.get_log_path() + '\n\n' +
+                           _('Some preferences are saved in ') + str(self.config.path_to_config()) + '\n' +
+                           _('Resources are loaded from ') + str(util.get_locale_dir().parent) + '\n' +
+                           _('The log is written to ') + str(util.get_log_path()) + '\n\n' +
                            _('Media support uses {}.').format(self.medias.backend_version) + '\n' +
                            _('Python version {}').format(sys.version))
         about.set_website('https://github.com/Cimbali/pympress')
@@ -774,12 +776,12 @@ class UI(builder.Builder):
             info (`int`): info on the target
             time (`int`): time of the drop
         """
-        received = data.get_text()
-        if received.startswith('file://'):
-            received = received[len('file://'):]
+        uri_parts = urlsplit(data.get_text())
+        path = pathlib.Path(url2pathname(uri_parts.path) if uri_parts.scheme else uri_parts.path)
+        scheme = uri_parts.scheme if uri_parts.scheme else 'file'
 
-        if os.path.isfile(received) and received.lower().endswith('.pdf'):
-            self.swap_document(os.path.abspath(received))
+        if path.is_file() and path.suffix.lower() == '.pdf':
+            self.swap_document(urlunsplit(uri_parts._replace(scheme=scheme, path=pathname2url(str(path.resolve())))))
 
 
     def unsaved_changes(self, reload=False):
@@ -888,17 +890,22 @@ class UI(builder.Builder):
         dialog.destroy()
 
 
-    def error_opening_file(self, filename):
+    def error_opening_file(self, uri):
         """ Remove the current document.
+
+        Args:
+            uri (`str`): the URI of the document
         """
         # Check if the path is valid
-        if not os.path.exists(filename):
+        uri_parts = urlsplit(uri)
+        filename = pathlib.Path(url2pathname(uri_parts.path))
+        if uri_parts.scheme == 'file' and not filename.exists():
             msg = _('Could not find the file "{}"').format(filename)
         else:
-            msg = _('Error opening the file "{}"').format(filename)
-        dialog = Gtk.MessageDialog(transient_for = self.p_win, flags = Gtk.DialogFlags.MODAL,
-                                   message_type = Gtk.MessageType.ERROR, message_format = msg)
-        dialog.add_buttons(Gtk.STOCK_CLOSE, Gtk.ResponseType.CLOSE)
+            msg = _('Error opening the file "{}"').format(uri)
+        dialog = Gtk.MessageDialog(transient_for=self.p_win, flags=Gtk.DialogFlags.MODAL,
+                                   message_type=Gtk.MessageType.ERROR, message_format=msg)
+        dialog.add_button(Gtk.STOCK_CLOSE, Gtk.ResponseType.CLOSE)
         dialog.set_position(Gtk.WindowPosition.CENTER)
         dialog.run()
         dialog.destroy()

pympress/util.py
@@ -33,6 +33,7 @@ import subprocess
 import importlib
 import os
 import sys
+import pathlib
 
 if not getattr(sys, 'frozen', False):
     # doesn’t play too well with cx_Freeze
@@ -99,39 +100,20 @@ def __get_resource_path(*path_parts):
         to the resource, relative to the pympress distribution
 
     Returns:
-        `str`: The path to the resource
+        :class:`~pathlib.Path`: The path to the resource
     """
     if getattr(sys, 'frozen', False):
-        return os.path.join(os.path.dirname(sys.executable), *path_parts)
+        return pathlib.Path(sys.executable).parent.joinpath(*path_parts)
     else:
         req = pkg_resources.Requirement.parse('pympress')
-        return pkg_resources.resource_filename(req, '/'.join(('pympress',) + path_parts))
-
-
-def __get_resource_list(*path_parts):
-    """ Return the list of elements in a directory based on whether its frozen or not.
-
-    Paths parts given should be relative to the pympress package dir.
-
-    Args:
-        name (`tuple` of `str`): The directories that constitute the path to the resource,
-        relative to the pympress distribution
-
-    Returns:
-        `list` of `str`: The paths to the resources in the directory
-    """
-    if getattr(sys, 'frozen', False):
-        return os.listdir(os.path.join(os.path.dirname(sys.executable), *path_parts))
-    else:
-        req = pkg_resources.Requirement.parse('pympress')
-        return pkg_resources.resource_listdir(req, '/'.join(('pympress',) + path_parts))
+        return pathlib.Path(pkg_resources.resource_filename(req, '/'.join(('pympress',) + path_parts)))
 
 
 def get_locale_dir():
     """ Returns the path to the locale directory.
 
     Returns:
-        str: The path to the locale directory
+        :class:`~pathlib.Path`: The path to the locale directory
     """
     return __get_resource_path('share', 'locale')
 
@@ -140,7 +122,7 @@ def get_portable_config():
     """ Returns the path to the configuration file for a portable install (i.e. in the install root).
 
     Returns:
-        str: The path to the portable configuration file.
+        :class:`~pathlib.Path`: The path to the portable configuration file.
     """
     return __get_resource_path('pympress.conf')
 
@@ -149,31 +131,31 @@ def get_default_config():
     """ Returns the path to the configuration file containing the defaults.
 
     Returns:
-        str: The path to the portable configuration file.
+        :class:`~pathlib.Path`: The path to the portable configuration file.
     """
     return __get_resource_path('share', 'defaults.conf')
 
 
 def get_user_config():
-    """ Returns the path to the configuration file in the user config directory.
+    """ Returns the path to the configuration file in the user config directory
 
     Returns:
-        `str`: path to the user configuration file.
+        :class:`~pathlib.Path`: path to the user configuration file.
     """
     if IS_WINDOWS:
-        base_dir = os.getenv('APPDATA')
+        base_dir = pathlib.Path(os.getenv('APPDATA'))
     elif IS_MAC_OS:
-        base_dir = os.path.expanduser('~/Library/Preferences')
+        base_dir = pathlib.Path('~/Library/Preferences').expanduser()
     else:
-        base_dir = os.getenv('XDG_CONFIG_HOME', os.path.expanduser('~/.config'))
-        if not os.path.isdir(base_dir):
-            os.mkdir(base_dir)
+        base_dir = pathlib.Path(os.getenv('XDG_CONFIG_HOME', '~/.config')).expanduser()
+        if not base_dir.exists():
+            base_dir.mkdir(parents=True)
 
-    return os.path.join(base_dir, 'pympress' + ('.ini' if IS_WINDOWS else ''))
+    return base_dir.joinpath('pympress' + ('.ini' if IS_WINDOWS else ''))
 
 
 def load_style_provider(style_provider):
-    """ Load the css and in a style provider.
+    """ Load the css and in a style provider
 
     Args:
         style_provider (:class:`~Gtk.CssProvider`): The style provider in which to load CSS
@@ -181,24 +163,24 @@ def load_style_provider(style_provider):
     Returns:
         :class:`~Gtk.CssProvider`: The style provider with CSS loaded
     """
-    style_provider.load_from_path(__get_resource_path('share', 'css', 'default.css'))
+    style_provider.load_from_path(str(__get_resource_path('share', 'css', 'default.css')))
     return style_provider
 
 
 def get_icon_path(name):
-    """ Load an image from pympress' resources in a Gdk Pixbuf.
+    """ Get the path for an image from pympress' resources
 
     Args:
         name (`str`): The name of the icon to load
 
     Returns:
-        :class:`~GdkPixbuf.Pixbuf`: The loaded icon
+        `str`: The path to the icon to load
     """
-    return __get_resource_path('share', 'pixmaps', name)
+    return str(__get_resource_path('share', 'pixmaps', name))
 
 
 def get_ui_resource_file(name, ext='.glade'):
-    """ Load an UI definition file from pympress' resources.
+    """ Load an UI definition file from pympress' resources
 
     Args:
         name (`str`): The name of the UI to load
@@ -207,7 +189,7 @@ def get_ui_resource_file(name, ext='.glade'):
     Returns:
         `str`: The full path to the glade file
     """
-    return __get_resource_path('share', 'xml', name + ext)
+    return str(__get_resource_path('share', 'xml', name + ext))
 
 
 def list_icons():
@@ -216,42 +198,40 @@ def list_icons():
     Returns:
         `list` of `str`: The paths to the icons in the pixmaps directory
     """
-    icons = __get_resource_list('share', 'pixmaps')
-
-    return [get_icon_path(i) for i in icons if os.path.splitext(i)[1].lower() == '.png' and i[:9] == 'pympress-']
+    return list(map(str, __get_resource_path('share', 'pixmaps').glob('pympress-*.png')))
 
 
 def get_log_path():
     """ Returns the appropriate path to the log file in the user app dirs.
 
     Returns:
-        `str`: path to the log file.
+        :class:`~pathlib.Path`: path to the log file.
     """
     if IS_WINDOWS:
-        base_dir = os.getenv('LOCALAPPDATA', os.getenv('APPDATA'))
+        base_dir = pathlib.Path(os.getenv('LOCALAPPDATA', os.getenv('APPDATA')))
     elif IS_MAC_OS:
-        base_dir = os.path.expanduser('~/Library/Logs')
+        base_dir = pathlib.Path('~/Library/Logs').expanduser()
     else:
-        base_dir = os.getenv('XDG_CACHE_HOME', os.path.expanduser('~/.cache'))
+        base_dir = pathlib.Path(os.getenv('XDG_CACHE_HOME', '~/.cache')).expanduser()
 
-    if not os.path.isdir(base_dir):
-        os.makedirs(base_dir)
+    if not base_dir.exists():
+        base_dir.mkdir(parents=True)
 
-    return os.path.join(base_dir, 'pympress.log')
+    return base_dir.joinpath('pympress.log')
 
 
 def fileopen(f):
     """ Call the right function to open files, based on the platform.
 
     Args:
-        f (`str`): path to the file to open
+        f (path-like): path to the file to open
     """
     if IS_WINDOWS:
         os.startfile(f)
     elif IS_MAC_OS:
-        subprocess.call(['open', f])
+        subprocess.call(['open', str(f)])
     else:
-        subprocess.call(['xdg-open', f])
+        subprocess.call(['xdg-open', str(f)])
 
 
 def hard_set_screensaver(disabled):

commit 6ccbacf7ad35900e4c557c12d61ee5b7f365b6b8

Fix file watching regression from 0c2dfbe..70c9185
Also do less passing *args and **kwargs objects around, simply save the
callback and path in the extras.FileWatcher state.

pympress/extras.py
@@ -704,6 +704,12 @@ class FileWatcher(object):
     # `int` that is a GLib timeout id to delay the callback
     timeout = 0
 
+    #: The :class:`~pathlib.Path` to the file being watched
+    path = None
+
+    #: Callback to be called on file changes, usually connected to :meth:`~pympress.ui.UI.reload_document`
+    callback = lambda: None
+
     def __init__(self):
         try:
             from watchdog.observers import Observer
@@ -744,16 +750,17 @@ class FileWatcher(object):
         self.stop_watching()
 
         scheme, path = uri.split('://', 1)
-        path = pathlib.Path(url2pathname(path))
+        self.path = pathlib.Path(url2pathname(path))
         if scheme != 'file':
             logger.error('Impossible to watch files with {} schemes'.format(scheme), exc_info = True)
             return
 
-        self.monitor.on_modified = lambda e: self._enqueue(callback, *args, **kwargs) if e.src_path == path else None
+        self.callback = lambda: callback(*args, **kwargs)
+        self.monitor.on_modified = self._enqueue
         try:
-            self.observer.schedule(self.monitor, str(path.parent))
+            self.observer.schedule(self.monitor, str(self.path.parent), recursive=False)
         except OSError:
-            logger.error('Impossible to open dir at {}'.format(str(path.parent)), exc_info = True)
+            logger.error('Impossible to open dir at {}'.format(str(self.path.parent)), exc_info = True)
 
 
     def stop_watching(self):
@@ -762,23 +769,22 @@ class FileWatcher(object):
         self.observer.unschedule_all()
 
 
-    def _enqueue(self, callback, *args, **kwargs):
-        """ Do not call callback directly, instead delay as to avoid repeated calls in short periods of time.
+    def _enqueue(self, event):
+        """ Call callback with delay, to avoid repeated calls in short periods of time.
 
         Args:
-            callback (`function`): callback to call with all the further arguments
+            event (:class:`~watchdog.events.FileSystemEvent`): the event that caused the callback to be triggered
         """
+        if event.src_path != str(self.path):
+            return
         if self.timeout:
             GLib.Source.remove(self.timeout)
-        self.timeout = GLib.timeout_add(200, self._call, callback, *args, **kwargs)
+        self.timeout = GLib.timeout_add(200, self._call)
 
 
-    def _call(self, callback, *args, **kwargs):
+    def _call(self):
         """ Call the callback.
-
-        Args:
-            callback (`function`): callback to call with all the further arguments
         """
         if self.timeout:
             self.timeout = 0
-        callback(*args, **kwargs)
+        self.callback()