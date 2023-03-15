a3c7f593fa08d604d4d29d188c05b9a96c9ad09f
Cimbali <me@cimba.li>
2023-03-15 20:01:19 +0000

    Allow building with a fallback theme

--- docs/conf.py
+++ docs/conf.py
@@ -244,19 +244,24 @@ intersphinx_mapping = {
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
 
 # Read the docs theme
-import sphinx_rtd_theme
+try:
+    import sphinx_rtd_theme
+except ImportError:
+    html_theme = "alabaster"
+else:
+    html_theme = "sphinx_rtd_theme"
+
+    # Add any paths that contain custom themes here, relative to this directory.
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
 # html_theme_options = {}
-
-# Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
